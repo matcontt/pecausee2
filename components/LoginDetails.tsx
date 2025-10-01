9d8d7e72bedab12769adb11c1e0b0b2883771d9f
alejandromateo186 <alejandromateo186@gmail.com>
2025-10-01 17:09:36 +0000

    ✨feat(LoginDetails): Componente para detalles de Login - Se puso de color blanco

---
+++ components/LoginDetails.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native'
+import React from 'react'
+
+const LoginDetails = () => {
+  return (
+    <View className="bg-white absolute bottom-0 h-[75%] w-full rounded-tl-[60px] p-8">
+        
+    </View>
+  )
+}
+
+export default LoginDetails
