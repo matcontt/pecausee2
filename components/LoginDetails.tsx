3fd4479901feb418598b24f5fa76c2d72a67e9c5
alejandromateo186 <alejandromateo186@gmail.com>
2025-10-01 17:15:35 +0000

    ♻️refactor(LoginDetails): Incluir campos de texto - Importar CustomTextInput en LoginDetails - Añadir campos de texto dentro de la vista

--- components/LoginDetails.tsx
+++ components/LoginDetails.tsx
@@ -1,12 +1,17 @@
 import { View } from 'react-native'
 import React from 'react'
 import CustomText from './CustomText'
+import CustomTextInput from './CustomTextInput'
 
 const LoginDetails = () => {
   return (
     <View className="bg-white absolute bottom-0 h-[75%] w-full rounded-tl-[60px] p-8 items-center">
         <CustomText variant='large' dark={false}>Welcom Back</CustomText>
         <CustomText variant='small' dark={false}>Login to your account</CustomText>
+        <View className="w-full mt-8">
+            <CustomTextInput placeholder='Full Name' iconName='user'/>
+            <CustomTextInput placeholder='••••••••' iconName='lock' isPassword={true}/>
+        </View>
     </View>
   )
 }
