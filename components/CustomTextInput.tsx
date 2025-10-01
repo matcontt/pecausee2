243c3219b40e92de17f61d2ae8177cb2f8ed8a99
alejandromateo186 <alejandromateo186@gmail.com>
2025-10-01 17:42:07 +0000

    ♻️refactor(CustomTextInput): Estado showPassword (true/false) - Se corrige la lógica para que al presionar el botón se pueda observar la contraseña

--- components/CustomTextInput.tsx
+++ components/CustomTextInput.tsx
@@ -1,5 +1,5 @@
-import { View, TextInput } from 'react-native'
-import React from 'react'
+import { View, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { Feather } from '@expo/vector-icons';
 
 interface CustomTextInputProps {
@@ -11,19 +11,23 @@ interface CustomTextInputProps {
 }
 
 const CustomTextInput = ({ placeholder, iconName, isPassword = false, value, onChangeText }: CustomTextInputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <View className="flex-row items-center bg-[#F0F4F3] rounded-xl p-4 my-3 w-full">
       <Feather name={iconName} size={20} color="#82A387" />
       <TextInput
         placeholder={placeholder}
         className="ml-3 flex-1 text-[#82A387]"
-        secureTextEntry={isPassword}
+        secureTextEntry={isPassword && !showPassword}
         placeholderTextColor="#82A387"
         value={value}
         onChangeText={onChangeText}
       />
       {isPassword && (
-        <Feather name="eye" size={20} color="#82A387" />
+        <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Feather name={showPassword ? "eye-off" : "eye"} size={20} color="#82A387" />
+        </TouchableOpacity>
       )}
     </View>
   )
