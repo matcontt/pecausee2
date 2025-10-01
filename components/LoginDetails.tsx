a69d22ef275b1da66edd8ba430618a3198b9d140
alejandromateo186 <alejandromateo186@gmail.com>
2025-10-01 17:11:46 +0000

    ✨feat(LoginDetails): Añadir textos y ajustar estilos  - Componentes de CustomText añadidos - Ajustamos props de CustomText

--- components/LoginDetails.tsx
+++ components/LoginDetails.tsx
@@ -1,10 +1,12 @@
 import { View } from 'react-native'
 import React from 'react'
+import CustomText from './CustomText'
 
 const LoginDetails = () => {
   return (
-    <View className="bg-white absolute bottom-0 h-[75%] w-full rounded-tl-[60px] p-8">
-        
+    <View className="bg-white absolute bottom-0 h-[75%] w-full rounded-tl-[60px] p-8 items-center">
+        <CustomText variant='large' dark={false}>Welcom Back</CustomText>
+        <CustomText variant='small' dark={false}>Login to your account</CustomText>
     </View>
   )
 }
