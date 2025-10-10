6c876abc6c16a43a59516b2e9d150ab4cd2dc70a
alejandromateo186 <alejandromateo186@gmail.com>
2025-10-10 01:34:22 +0000

    💄ui(/CustomButton): Se cambio de color al boton

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -8,7 +8,7 @@ interface CustomButtonProps {
 
 const CustomButton = ({ label, onPress }: CustomButtonProps) => {
   return (
-    <TouchableOpacity onPress={onPress} className="bg-[#000000] rounded-full p-4 w-full items-center mt-8">
+    <TouchableOpacity onPress={onPress} className="bg-[#b11f32] rounded-full p-4 w-full items-center mt-8">
       <Text className="text-white text-lg">{label}</Text>
     </TouchableOpacity>
   )
